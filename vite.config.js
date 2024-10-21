b5bd2e84cceda9be1bc0cc3938e8bb88e772d869
Nestorfron <nestorfrones07@hotmail.com>
2024-10-20 22:22:00 -0300

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  base: './',
+  base: './Vite-Nextui-Flask',
   plugins: [react()],
   build: {
     assetsDir: 'static',
